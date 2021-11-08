cbfcb693dddc21168be20156654687454e4ab186
Martin Dwyer <martin.dwyer@outlook.com>
2021-11-08 12:15:02 -0600

    Core updates

---
+++ NodeWeatherApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NodeWeatherApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
